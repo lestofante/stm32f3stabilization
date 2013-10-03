7444bb0f68ecd7bc0770620bc7e9032cccac7d5c
lestofante <mombelli.mauro@gmail.com>
2013-10-03 21:57:39 +0200

    USB is optional

--- src/usb_main.c
+++ src/usb_main.c
@@ -14,11 +14,18 @@ __IO uint8_t READY = 1;
 __IO uint8_t DATA_PRESENT = 0;
 
 uint16_t seqN = 0;
+uint8_t funziona = 0;
 
 void USB_write(uint8_t* toWrite, uint16_t size, enum packet_type_t raw_type){
 
-	if(size>63-4) size = 63-4;
-	if(size<0) size = 0;
+	if (!funziona)
+		return;
+
+	if(size>63-4)
+		size = 63-4;
+
+	if(size<0)
+		size = 0;
 
 	while(!READY)
 		;	//wait for read to occur
@@ -63,4 +70,5 @@ void USB_Config(void)
 
 	while (bDeviceState != CONFIGURED)
 	{}
+	funziona = 1;
 }
